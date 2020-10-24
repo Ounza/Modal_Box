735d253b8fad850ccf1e66e0613e29f38b95608e
Rush Morgan <51529432+Ounza@users.noreply.github.com>
2020-10-24 16:35:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal');
+const open = document.getElementById('open');
+
+//Add an evemt listener
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+});
+
+//Show modal
+open.addEventListener('click', () =>{
+    modal.classList.add('show-modal')
+});
+//Hide modal
+close.addEventListener('click', () =>{
+    modal.classList.remove('show-modal')
+}); 
+
+//Hide modal on outside click
+window.addEventListener('click', e => {
+    e.target==modal? modal.classList.remove('show-modal') : false
+});
